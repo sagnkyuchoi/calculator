89fc521691103a6c6905b7d754c56e3b86c0f851
MF839-012 <mf839-012@70-12-240-93.pools.spcsdns.net>
2016-05-16 17:16:01 +0900

    first commit

--- FractionCalculator/ViewController.h
+++ FractionCalculator/ViewController.h
@@ -10,6 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+- (IBAction)clickDigit:(id)sender;
+- (IBAction)clickOver:(id)sender;
+- (IBAction)clear:(id)sender;
+- (IBAction)clickEquals:(id)sender;
+- (IBAction)clickMinus:(id)sender;
+- (IBAction)clickPlus:(id)sender;
+- (IBAction)clickMultiply:(id)sender;
+- (IBAction)clickDivide:(id)sender;
+
+-(void)processDigit:(int)digit;
+-(void)processOp:(char)theOp;
+-(void)storeFracPart;
 
 @end
 
